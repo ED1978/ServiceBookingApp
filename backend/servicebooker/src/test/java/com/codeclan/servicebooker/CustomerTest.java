d0d34aea16016a020f0e296f892c6d68c42fbe28
EDownie <53910027+ED1978@users.noreply.github.com>
2019-11-29 17:33:09 +0000

    User methods tested

--- backend/servicebooker/src/test/java/com/codeclan/servicebooker/CustomerTest.java
+++ backend/servicebooker/src/test/java/com/codeclan/servicebooker/CustomerTest.java
@@ -26,4 +26,59 @@ public class CustomerTest {
         customer1.setName("Dave Smith");
         assertEquals("Dave Smith", customer1.getName());
     }
+
+    @Test
+    public void canGetAddress() {
+        assertEquals("77 Renfrew Street", customer1.getAddress());
+    }
+
+    @Test
+    public void canSetCustomerAddress() {
+        customer1.setAddress("56 Union Street");
+        assertEquals("56 Union Street", customer1.getAddress());
+    }
+
+    @Test
+    public void hasEmail() {
+        assertEquals("john@gmail.com", customer1.getEmail());
+    }
+
+    @Test
+    public void setEmail() {
+        customer1.setEmail("john_s@gmail.com");
+        assertEquals("john_s@gmail.com", customer1.getEmail());
+    }
+
+    @Test
+    public void getTellNo() {
+        assertEquals("07898544265", customer1.getTellNo());
+    }
+
+    @Test
+    public void setTellNo() {
+        customer1.setTellNo("07898544267");
+        assertEquals("07898544267", customer1.getTellNo());
+    }
+
+    @Test
+    public void getLocation() {
+        assertEquals("Glasgow", customer1.getLocation());
+    }
+
+    @Test
+    public void setLocation() {
+        customer1.setLocation("Edinburgh");
+        assertEquals("Edinburgh", customer1.getLocation());
+    }
+
+    @Test
+    public void getRating() {
+        assertEquals(0.00, customer1.getRating(), 0.01);
+    }
+
+    @Test
+    public void setRating() {
+        customer1.setRating(1.99);
+        assertEquals(1.99, customer1.getRating(), 0.01);
+    }
 }
