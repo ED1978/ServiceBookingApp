1b19e5568c4192a942819e823d513d6fc56304de
EDownie <53910027+ED1978@users.noreply.github.com>
2019-11-29 13:11:11 +0000

    Basic freelancer created

---
+++ backend/servicebooker/src/main/java/com/codeclan/servicebooker/models/freelancers/Freelancer.java
@@ -0,0 +1,25 @@
+package com.codeclan.servicebooker.models.freelancers;
+
+import com.codeclan.servicebooker.models.User;
+
+public class Freelancer extends User {
+
+    private String category;
+
+    public Freelancer(String name, String address, String email, String tellNo, String location, String category){
+        super(name, address, email, tellNo, location);
+        this.category = category;
+    }
+
+    public Freelancer(){
+
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
