6d37b2f7fa5a60578d8dc86aca5380a48a266342
EDownie <53910027+ED1978@users.noreply.github.com>
2019-11-29 12:41:31 +0000

    Backend Initialised

---
+++ backend/servicebooker/src/main/java/com/codeclan/servicebooker/ServicebookerApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.servicebooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicebookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicebookerApplication.class, args);
+	}
+
+}
---
+++ backend/servicebooker/src/test/java/com/codeclan/servicebooker/ServicebookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.servicebooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicebookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
