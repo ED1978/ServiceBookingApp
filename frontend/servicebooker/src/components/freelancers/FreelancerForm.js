ab33342e3f7b44a0e07bca933327a332ceea530e
EDownie <53910027+ED1978@users.noreply.github.com>
2019-12-01 20:38:55 +0000

    can add new freelancer

--- frontend/servicebooker/src/components/freelancers/FreelancerForm.js
+++ frontend/servicebooker/src/components/freelancers/FreelancerForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const FreelancerForm = (props) => {
+  console.log(props)
 
   function handleSubmit(event){
     event.preventDefault();
@@ -12,17 +13,19 @@ const FreelancerForm = (props) => {
       location: event.target.location.value,
       category: event.target.category.value
     }
+    props.handlePost(freelancer);
   }
 
   return(
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Name" name="name"/>
         <input type="text" placholder="Address" name="address"/>
         <input type="text" placeholder="Email" name="email"/>
         <input type="text" placeholder="Telephone Number" name="telNo"/>
         <input type="text" placeholder="Location" name="location"/>
         <input type="text" placeholder="Category" name="category"/>
+        <button type="submit">Save</button>
       </form>
     </div>
   )
