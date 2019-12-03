b6c81edc622125fb6a55a6c4cca801233c9d3b07
EDownie <53910027+ED1978@users.noreply.github.com>
2019-12-03 23:09:30 +0000

    can edit customer account details

--- frontend/servicebooker/src/components/customers/CustomerEditForm.js
+++ frontend/servicebooker/src/components/customers/CustomerEditForm.js
@@ -5,9 +5,22 @@ const CustomerEditForm = (props) => {
     return "Loading...."
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const customer = {
+      name: event.target.name.value,
+      address: event.target.address.value,
+      email: event.target.email.value,
+      tellNo: event.target.tellNo.value,
+      location: event.target.location.value,
+      password: event.target.password.value
+    }
+    props.handleUpdate(customer);
+  }
+
   return(
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
       <input type="text" defaultValue={props.customer.name} name="name"/>
       <input type="text" defaultValue={props.customer.address} name="address"/>
       <input type="text" defaultValue={props.customer.email} name="email"/>
