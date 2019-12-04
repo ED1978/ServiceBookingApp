262cbbfc35663ce730d3318374f1214392820405
EDownie <53910027+ED1978@users.noreply.github.com>
2019-12-04 11:33:10 +0000

    basic styling navbar

--- frontend/servicebooker/src/NavBar.js
+++ frontend/servicebooker/src/NavBar.js
@@ -1,29 +1,35 @@
 import React from 'react';
+import './NavBar.css'
 
 
 const NavBar = (props) => {
   return(
     <header>
-      <ul>
-        <li>
-          <a href='/customers'>Customers</a>
-        </li>
-        <li className="nav-link">
-          <a href="/customers/new">Bocome a Customer</a>
-        </li>
-        <li className="nav-link">
-          <a href="/freelancers">Freelancers</a>
-        </li>
-        <li className="nav-link">
-          <a href="/freelancers/new">Become a Freelancer</a>
-        </li>
-        <li className="nav-link">
-          <a href="/jobs">Jobs</a>
-        </li>
-        <li className="nav-link">
-          <a href="/login">Login</a>
-        </li>
-      </ul>
+      <div className="title">
+        <h2>Find A Freelancer</h2>
+      </div>
+      <div className="nav-links">
+        <ul>
+          <li className="nav-link">
+            <a href='/customers'>Customers</a>
+          </li>
+          <li className="nav-link">
+            <a href="/customers/new">Become a Customer</a>
+          </li>
+          <li className="nav-link">
+            <a href="/freelancers">Freelancers</a>
+          </li>
+          <li className="nav-link">
+            <a href="/freelancers/new">Become a Freelancer</a>
+          </li>
+          <li className="nav-link">
+            <a href="/jobs">Jobs</a>
+          </li>
+          <li className="nav-link">
+            <a href="/login">Login</a>
+          </li>
+        </ul>
+      </div>
     </header>
   )
 }
